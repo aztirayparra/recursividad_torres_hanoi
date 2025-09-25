b4daa13a204db98f2a4b8fc493d5d947127b8267
aztirayparra <ylilianaparra@ucundinamarca.edu.co>
2025-09-25 07:38:20 -0500

    control de cantidad de discos

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,20 +6,28 @@ def hanoi(n_discos,t_inicial,t_auxiliar,t_final):
         hanoi(n_discos-1,t_inicial,t_final,t_auxiliar)
         print(f"movimiento disco {n_discos} de Torre {t_inicial} a Torre {t_final}")
         hanoi(n_discos-1,t_auxiliar,t_inicial,t_final)
+    
 
 def n_movimientos(discos):
     return (2**discos)-1
 while True:
     try:
         discos=int(input("Ingrese el número de discos:"))
+    except ValueError:
+        print("Se debe ingresar un valor numerico")
+    if discos==0:
+            print("No hay discos, así que no se puede realizar algún proceso, intenta de nuevo")
+    elif discos>20:
+            print("La cantidad de discos es demasiada, intenta con un valor más pequeño(1-20)")
+    else:
         torrei=input("ingrese el nombre de la torre inicial: ") or "A"
         torreaux=input("ingrese el nombre de la torre auxiliar: ") or "B"
         torref=input("ingrese el nombre de la torre final: ") or"c"
         break
-    except ValueError:
-        print("Se debe ingresar los datos correctamente")
  
 hanoi(discos,torrei,torreaux,torref)
 print(f"El numero de movimientos es:{n_movimientos(discos)}")
+
+
 
 
